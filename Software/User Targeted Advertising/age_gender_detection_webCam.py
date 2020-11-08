d6fecffeb80b04e0df2ecebe53df31c9c67a2a81
Hans Thisanke <59741895+hansthisanke@users.noreply.github.com>
2020-11-08 17:47:54 +0530

    Update age_gender_detection_webCam.py

--- Software/User Targeted Advertising/age_gender_detection_webCam.py
+++ Software/User Targeted Advertising/age_gender_detection_webCam.py
@@ -13,18 +13,18 @@ age_list = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)', '(25, 32)', '(38, 43)', '
 gender_list = ['Male', 'Female']
 
 #read pre-trained neural network data from path to variables
-protoPathAge = os.path.sep.join([r"C:\Users\hp\Desktop\AgeGenderDeepLearning-master\models",  "deploy_age.prototxt"])
-modelPathAge = os.path.sep.join([r"C:\Users\hp\Desktop\AgeGenderDeepLearning-master\models","age_net.caffemodel"])
+protoPathAge = os.path.sep.join([r"PATH",  "deploy_age.prototxt"])
+modelPathAge = os.path.sep.join([r"PATH","age_net.caffemodel"])
 
-protoPathGender = os.path.sep.join([r"C:\Users\hp\Desktop\AgeGenderDeepLearning-master\models",  "deploy_gender.prototxt"])
-modelPathGender = os.path.sep.join([r"C:\Users\hp\Desktop\AgeGenderDeepLearning-master\models","gender_net.caffemodel"])
+protoPathGender = os.path.sep.join([r"PATH",  "deploy_gender.prototxt"])
+modelPathGender = os.path.sep.join([r"PATH","gender_net.caffemodel"])
 
 #load neural network pre-trained models for detection
 age_net = cv2.dnn.readNetFromCaffe(protoPathAge,modelPathAge)
 gender_net = cv2.dnn.readNetFromCaffe(protoPathGender,modelPathGender)
 
 #load pre-build model for face detection
-face_cascade_path = os.path.sep.join([r"C:\Users\hp\Desktop\AgeGenderDeepLearning-master\haarcascade",  "haarcascade_frontalface_default.xml"])
+face_cascade_path = os.path.sep.join([r"PATH",  "haarcascade_frontalface_default.xml"])
 face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
 while cap.isOpened():
